1d9a2586aabe55caa8886485a7af16e0dcfac7e3
Patrick <pjm3vm@virginia.edu>
2016-10-01 19:17:30 -0400

    -Fixed urls to include "v1" -GETs for daddy/baby lists return dictionaries instead of arrays -Fixed index.html to properly load js and css files

--- sugar/sugarapp/views.py
+++ sugar/sugarapp/views.py
@@ -11,14 +11,24 @@ class BabyDetail(generics.RetrieveUpdateDestroyAPIView):
 class BabyList(generics.ListCreateAPIView):
     queryset = Baby.objects.all()
     serializer_class = BabySerializer
+    def list(self, request, *args, **kwargs):
+        response = super(BabyList, self).list(request, *args, **kwargs) # call the original 'list'
+        response.data = {"babbies": response.data} # customize the response data
+        return response # return response with this custom representation
 
 class DaddyDetail(generics.RetrieveUpdateDestroyAPIView):
+
     queryset = Daddy.objects.all()
     serializer_class = DaddySerializer
 
 class DaddyList(generics.ListCreateAPIView):
     queryset = Daddy.objects.all()
     serializer_class = DaddySerializer
+
+    def list(self, request, *args, **kwargs):
+        response = super(DaddyList, self).list(request, *args, **kwargs) # call the original 'list'
+        response.data = {"daddies": response.data} # customize the response data
+        return response # return response with this custom representation
 
 def index(request):
     return render(request, 'babies/index.html')
