c7cf61727d43a94302205faf1b0bc6d79b3d296e
Patrick <pjm3vm@virginia.edu>
2016-10-05 20:48:45 -0400

    -Added unit tests

--- entities/entitiesapp/tests.py
+++ entities/entitiesapp/tests.py
@@ -4,18 +4,39 @@ from entitiesapp.models import Baby, Daddy
 
 class EntitiesTestCase(TestCase):
     def setUp(self):
-        pass
+        Daddy.objects.create(id=1, first_name='sugar', last_name='daddy', income='100', city='Fairfax', state='VA')
+        Baby.objects.create(id=1, first_name='sugar', last_name='daddy', city='Fairfax', state='VA')
+
 
     def test_daddy(self):
-        response = self.client.get(reverse('get_daddy', kwargs={'user_id':1}))
+        response = self.client.get(reverse('get_daddies_list')+"1/")
         self.assertContains(response, 'id')
 
     def test_daddies_list(self):
+        print(reverse('get_daddies_list'))
         response = self.client.get(reverse('get_daddies_list'))
+
         self.assertContains(response, 'daddies')
 
-    def test_daddies_fields(self):
-        response = self.client.get(reverse('get_daddies_list'))
+    def test_daddies_income(self):
+        response = self.client.get(reverse('get_daddies_list')+"1/")
+        self.assertContains(response, 'income')
+
+    def test_daddies_state(self):
+        response = self.client.get(reverse('get_daddies_list')+"1/")
+        self.assertContains(response, 'state')
+
+    def test_daddies_city(self):
+        response = self.client.get(reverse('get_daddies_list')+"1/")
+        self.assertContains(response, 'city')
+
+    def test_babies(self):
+        response = self.client.get(reverse('get_babies_list'))
+        self.assertContains(response, 'babies')
+
+    def test_babies_name(self):
+        response = self.client.get(reverse('get_babies_list')+"1/")
+        self.assertContains(response, 'first_name')
 
     def tearDown(self):
         pass
