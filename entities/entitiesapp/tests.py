3201c23206077988353401f343c3174a6327350b
Ryan <rdp4wq@virginia.edu>
2016-10-05 20:22:52 -0400

    Tests

--- entities/entitiesapp/tests.py
+++ entities/entitiesapp/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from entitiesapp.models import Baby, Daddy
 
-# Create your tests here.
+class EntitiesTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_daddy(self):
+        response = self.client.get(reverse('get_daddy', kwargs={'user_id':1}))
+        self.assertContains(response, 'id')
+
+    def test_daddies_list(self):
+        response = self.client.get(reverse('get_daddies_list'))
+        self.assertContains(response, 'daddies')
+
+    def test_daddies_fields(self):
+        response = self.client.get(reverse('get_daddies_list'))
+
+    def tearDown(self):
+        pass
--- entities/entitiesapp/urls.py
+++ entities/entitiesapp/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 import entitiesapp.views as views
 
 urlpatterns = [
-    url(r'^api/v1/babies/(?P<pk>[0-9]+)/$', views.BabyDetail.as_view()),
-    url(r'^api/v1/babies/$', views.BabyList.as_view()),
-    url(r'^api/v1/daddies/(?P<pk>[0-9]+)/$', views.DaddyDetail.as_view()),
-    url(r'^api/v1/daddies/$', views.DaddyList.as_view()),
+    url(r'^api/v1/babies/(?P<pk>[0-9]+)/$', views.BabyDetail.as_view(), name='get_baby'),
+    url(r'^api/v1/babies/$', views.BabyList.as_view(), name='get_babies_list'),
+    url(r'^api/v1/daddies/(?P<pk>[0-9]+)/$', views.DaddyDetail.as_view(), name='get_daddy'),
+    url(r'^api/v1/daddies/$', views.DaddyList.as_view(), name='get_daddies_list'),
     url(r'^$', views.index)
 ]
