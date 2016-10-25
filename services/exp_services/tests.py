080750bcc727a1d11482435a0e331c4b76718e69
Ryan <rdp4wq@virginia.edu>
2016-10-24 22:11:32 -0400

    Services login tests

--- services/exp_services/tests.py
+++ services/exp_services/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.http import Http404
 
 # Create your tests here.
+class ServicesTestCase(TestCase):
+    def test_valid_login(self):
+        response = self.client.post(reverse('login'), {'username': 'Ry', 'password': 'password'})
+        self.assertContains(response, 'authenticator')
+
+    def test_invalid_login(self):
+        response = self.client.post(reverse('login'), {'username': 'Ry', 'password': 'not_the_right_password'})
+        self.assertEqual(response.status_code, 404)
--- services/exp_services/urls.py
+++ services/exp_services/urls.py
@@ -8,10 +8,10 @@ urlpatterns = [
     url(r'^api/v1/services/babies/(?P<pk>[0-9]+)/$', views.get_baby_from_models, name='baby_detail'),
     url(r'^api/v1/services/daddies/$', views.get_daddies_from_models, name='daddies_all'),
     url(r'^api/v1/services/daddies/(?P<pk>[0-9]+)/$', views.get_daddy_from_models, name='daddy_detail'),
-    url(r'^api/v1/login/$', views.login),
-    url(r'^api/v1/authenticate/$', views.authenticate),
+    url(r'^api/v1/login/$', views.login, name='login'),
+    url(r'^api/v1/authenticate/$', views.authenticate, name='authenticate'),
     url(r'api/v1/dates/new$', views.create_date, name='date_new'),
-    url(r'api/v1/dates/', views.get_dates),
+    url(r'api/v1/dates/', views.get_dates, name='dates'),
     url(r'api/v1/register/', views.create_user, name='user_new'),
 
 ]
